c1fedb575cb53109e8499a144b44a66995c20c85
Jeff Harrell <jeff@juxtadesign.com>
2014-10-08 16:06:36 -0700

    Flipping PayPal 2014 with Intuit 2014 sites

--- js/app.js
+++ js/app.js
@@ -2,30 +2,22 @@
  * Created by lmarkus on 1/10/14.
  */
 $(function () {
-    $(window).resize(function () {
-        if ($(window).width() > 640) {
-            $('.nav').removeClass('tc').show();
-            $('.toggler').hide();
-        }
-        else {
-            $('.nav').hide();
-            $('.toggler').show();
-        }
+    // $(window).resize(function () {
+    //     if ($(window).width() > 640) {
+    //         $('.nav').removeClass('tc').show();
+    //         $('.toggler').hide();
+    //     }
+    //     else {
+    //         $('.nav').hide();
+    //         $('.toggler').show();
+    //     }
+    // });
+
+    $('.navbar-toggle').on('click', function () {
+        $(this).toggleClass('active');
     });
 
-    $('.toggler').click(function () {
-        var nav = $('.nav');
-        if (nav.hasClass('tc')) {
-            nav.toggle();
-        }
-        else {
-            nav.addClass('tc');
-        }
-    })
-
     $('.nav li').click(function () {
-        if ($(window).width() < 640) {
-            $('.nav').hide();
-        }
+        $('.navbar-toggle').click();
     });
 });
